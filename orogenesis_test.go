99295cd9b2d242de0228049760071bc3b4d492b9
Nat Wilson <njwilson23@gmail.com>
2015-02-01 16:41:44 -0800

    add test for creating a page

--- orogenesis_test.go
+++ orogenesis_test.go
@@ -2,28 +2,38 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"testing"
 )
 
 func TestRawSources(t *testing.T) {
 	page := Page{"tests/example-template.html",
-		"", "<b>Title</b>", // title
+		"", "<b>Title fragment</b>", // title
 		"", "<link href=\"example.css\" rel=\"stylesheet\">\n<!-- Javascript examples -->", // header
 		"", "<p>Test with raw sources</p>", // body
 		"", "<div class=\"centered\">This content is in the Public Domain</div>", // footer
 		"tests/nofile.html", // output path
 	}
 
-	title := page.Title()
-	header := page.Header()
-	body := page.Body()
-	footer := page.Footer()
+	if string(page.Title()) != "<b>Title fragment</b>" {
+		fmt.Println(page.Title())
+		t.Fail()
+	}
 
-	fmt.Println(title)
-	fmt.Println(header)
-	fmt.Println(body)
-	fmt.Println(footer)
+	if string(page.Header()) != "<link href=\"example.css\" rel=\"stylesheet\">\n<!-- Javascript examples -->" {
+		fmt.Println(page.Header())
+		t.Fail()
+	}
 
+	if string(page.Body()) != "<p>Test with raw sources</p>" {
+		fmt.Println(page.Body())
+		t.Fail()
+	}
+
+	if string(page.Footer()) != "<div class=\"centered\">This content is in the Public Domain</div>" {
+		fmt.Println(page.Footer())
+		t.Fail()
+	}
 }
 
 func TestPathSources(t *testing.T) {
@@ -35,14 +45,44 @@ func TestPathSources(t *testing.T) {
 		"tests/nofile.html", // output path
 	}
 
-	title := page.Title()
-	header := page.Header()
-	body := page.Body()
-	footer := page.Footer()
+	if string(page.Title()) != "<b>Title fragment</b>\n" {
+		t.Fail()
+	}
 
-	fmt.Println(title)
-	fmt.Println(header)
-	fmt.Println(body)
-	fmt.Println(footer)
+	if string(page.Header()) != "<link href=\"example.css\" rel=\"stylesheet\">\n<!-- Javascript examples -->\n" {
+		t.Fail()
+	}
 
+	if string(page.Body()) != "<p>Test with path sources</p>\n" {
+		t.Fail()
+	}
+
+	if string(page.Footer()) != "<div class=\"centered\">This content is in the Public Domain</div>\n" {
+		t.Fail()
+	}
+}
+
+func TestPageConstruction(t *testing.T) {
+	page, err := ReadConfig("tests/example.yaml")
+	if err != nil {
+		fmt.Println(err)
+		t.Fail()
+	}
+
+	// Write output
+	fout, err := os.Create("test.html")
+	if err != nil {
+		fmt.Println(err)
+		t.Fail()
+	}
+
+	err = BuildPage("tests", fout, page)
+	if err != nil {
+		fmt.Println(err)
+		t.Fail()
+	}
+
+	if page == nil {
+		t.Fail()
+	}
 }
